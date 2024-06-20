2c993359a686490d56a42359d38c1eb4f4188e48
dishakshirsagar <dishakshirsagar@gmail.com>
2024-06-20 22:43:14 +0530

    saved

---
+++ 38-alertbox/3801.dart
@@ -0,0 +1,97 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      home: adbox(),
+    );
+  }
+}
+
+class adbox extends StatefulWidget {
+  const adbox({super.key});
+
+  @override
+  State<adbox> createState() => _adboxState();
+}
+
+class _adboxState extends State<adbox> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        backgroundColor: Colors.amber,
+        title: Text(
+          "Alert Demo",
+          style: TextStyle(
+            fontWeight: FontWeight.bold,
+            fontSize: 30
+          ),
+          ),
+        ),
+        body:Center(
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.start,
+            children: [
+              ElevatedButton(onPressed: (){
+                showDialog(context: context,
+                 builder: (BuildContext context){
+                  return AlertDialog(
+                    title: Text("Alert"),
+                    content: Text("Do you want to Exit"),
+                    actions: [
+                      TextButton(onPressed: (){
+                        Navigator.of(context).pop();
+                      }, child: Text("OK",
+                       style: TextStyle(
+                color: Colors.black
+              ),),
+                      style: TextButton.styleFrom(
+                        backgroundColor: Colors.green,
+                        shape: BeveledRectangleBorder(
+                          borderRadius: BorderRadius.circular(3)
+                        )
+                      ),
+                  ),
+                  TextButton(onPressed:(){
+                    Navigator.of(context).pop();
+                  } , child: Text(
+                    "cancel",
+                     style: TextStyle(
+                color: Colors.black
+              ),
+                  ),
+                  style: TextButton.styleFrom(
+                    backgroundColor: Colors.green,
+                     shape: BeveledRectangleBorder(
+                          borderRadius: BorderRadius.circular(3)
+                        )
+                  ),)
+                    ],
+                  );
+                 });
+              }, child: Text("Show Dialog",
+              style: TextStyle(
+                color: Colors.white
+              ),),
+              style: ElevatedButton.styleFrom(
+                backgroundColor:  Color.fromARGB(255, 52, 5, 56),
+                 shape: BeveledRectangleBorder(
+                          borderRadius: BorderRadius.circular(3)
+                        )
+              ),),
+              
+            ],
+          ),
+        )
+      );
+  
+  }
+}
